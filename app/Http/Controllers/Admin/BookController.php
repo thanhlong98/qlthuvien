66e3f6046c4af5a3cf5f466250d21a0ad998c552
hoangnguyen <nvanhoang188@gmail.com>
2019-11-26 19:31:55 +0700

    chức năng mượn trả sách p1

--- app/Http/Controllers/Admin/BookController.php
+++ app/Http/Controllers/Admin/BookController.php
@@ -6,13 +6,17 @@ use App\Models\Issue;
 use App\Models\User;
 use App\Models\Book;
 use App\Models\Admin;
+use Carbon\Carbon;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
     public function issueBook() {
-        return view('admin.issue-book');
+        $users = User::select('*')->get()->toArray();
+        $books = Book::select('*')->get()->toArray();
+        $date = Carbon::now()->format('Y-m-d');
+        return view('admin.issue-book', compact('users', 'books', 'date'));
     }
 
     public function issuedBooks(Request $request) {
--- resources/views/admin/issue-book.blade.php
+++ resources/views/admin/issue-book.blade.php
@@ -5,5 +5,67 @@ Issue Book
 @endsection
 
 @section('content')
-issue book
+
+<form action="" method="post" class="row">
+    <div class="col-4">
+        <div class="form-group">
+            <label for="docgia">Mã độc giả</label>
+            <input
+                type="text"
+                class="form-control"
+                id="docgia"
+                name="docgia"
+                list="dsdocgia"
+                aria-describedby="docgia"
+                placeholder="Nhập mã độc giả">
+            <datalist id="dsdocgia">
+                @foreach($users as $user)
+                    <option value="{{$user['id']}}">{{$user['name']}}</option>
+                @endforeach
+            </datalist>
+        </div>
+        <div class="form-group">
+            <label for="sach">Mã sách</label>
+            <input
+                type="text"
+                class="form-control"
+                id="sach"
+                name="sach"
+                list="dssach"
+                aria-describedby="sach"
+                placeholder="Nhập mã sách">
+            <datalist id="dssach">
+                @foreach($books as $book)
+                    <option value="{{$book['id']}}">{{$book['name']}}</option>
+                @endforeach
+            </datalist>
+        </div>
+        <div class="form-group">
+            <label for="issueDate">Ngày mượn</label>
+            <input
+                type="date"
+                class="form-control"
+                id="issueDate"
+                name="issueDate"
+                aria-describedby="issueDate"
+                min="{{$date}}"
+                value="{{$date}}" >
+        </div>
+        <div class="form-group">
+            <label for="ghichu">Ghi chú</label>
+            <input
+                type="text"
+                class="form-control"
+                id="ghichu"
+                name="ghichu"
+                aria-describedby="ghichu"
+                placeholder="Nhập mã sách" />
+        </div>
+        <div class="form-group">
+            <button type="submit" class="btn btn-primary">Cho mượn</button>
+            <button type="reset" class="btn btn-secondary">Hủy</button>
+        </div>
+    </div>
+</form>
+
 @endsection
