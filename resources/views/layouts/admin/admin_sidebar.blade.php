c4ddeee38e88bb7d21cdbfad5615fe83d1debfa7
hoangnguyen <nvanhoang188@gmail.com>
2019-11-26 20:50:14 +0700

    đổi ngôn ngữ giao diện sang tiếng Việt

--- resources/views/layouts/admin/admin_sidebar.blade.php
+++ resources/views/layouts/admin/admin_sidebar.blade.php
@@ -15,7 +15,7 @@
   <li class="nav-item active">
     <a class="nav-link" href="{{ url('/admin/dashboard') }}">
       <i class="fas fa-fw fa-tachometer-alt"></i>
-      <span>Dashboard</span></a>
+      <span>Bảng điều khiển</span></a>
   </li>
 
   <!-- Divider -->
@@ -23,27 +23,87 @@
 
   <!-- Heading -->
   <div class="sidebar-heading">
-    Interface
+    Chức năng
   </div>
 
   <!-- Nav Item - Pages Collapse Menu -->
   <li class="nav-item">
-    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
       <i class="fas fa-book"></i>
-      <span>Book Issue</span>
+      <span>Quản lý sách</span>
+    </a>
+    <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionSidebar">
+      <div class="bg-white py-2 collapse-inner rounded">
+        <a class="collapse-item" href="{{ route('admin.issue-book') }}">Thêm sách mới</a>
+        <a class="collapse-item" href="{{ route('admin.issued-books') }}">Danh sách sách</a>
+      </div>
+    </div>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+      <i class="fas fa-tags"></i>
+      <span>Quản lý thể loại</span>
     </a>
     <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
       <div class="bg-white py-2 collapse-inner rounded">
+        <a class="collapse-item" href="{{ route('admin.issue-book') }}">Thêm thể loại mới</a>
+        <a class="collapse-item" href="{{ route('admin.issued-books') }}">Danh sách thể loại</a>
+      </div>
+    </div>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseThree" aria-expanded="true" aria-controls="collapseThree">
+      <i class="fas fa-retweet"></i>
+      <span>Quản lý mượn trả</span>
+    </a>
+    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionSidebar">
+      <div class="bg-white py-2 collapse-inner rounded">
         {{-- <h6 class="collapse-header">Custom Components:</h6> --}}
-      <a class="collapse-item" href="{{ route('admin.issue-book') }}">Issue A Book</a>
-        <a class="collapse-item" href="{{ route('admin.issued-books') }}">Issued Books</a>
-        <a class="collapse-item" href="{{ route('admin.non-return-book') }}">Non-return Books</a>
+      <a class="collapse-item" href="{{ route('admin.issue-book') }}">Đăng ký mượn</a>
+        <a class="collapse-item" href="{{ route('admin.issued-books') }}">Danh sách mượn trả</a>
+        <a class="collapse-item" href="{{ route('admin.non-return-book') }}">Danh sách quá hạn</a>
+      </div>
+    </div>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseFour" aria-expanded="true" aria-controls="collapseFour">
+      <i class="fas fa-book-reader"></i>
+      <span>Quản lý độc giả</span>
+    </a>
+    <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordionSidebar">
+      <div class="bg-white py-2 collapse-inner rounded">
+        <a class="collapse-item" href="{{ route('admin.issue-book') }}">Thêm độc giả mới</a>
+        <a class="collapse-item" href="{{ route('admin.issued-books') }}">Danh sách độc giả</a>
+      </div>
+    </div>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseFive" aria-expanded="true" aria-controls="collapseFive">
+      <i class="fas fa-pen-alt"></i>
+      <span>Quản lý tác giả</span>
+    </a>
+    <div id="collapseFive" class="collapse" aria-labelledby="headingFive" data-parent="#accordionSidebar">
+      <div class="bg-white py-2 collapse-inner rounded">
+        <a class="collapse-item" href="{{ route('admin.issue-book') }}">Thêm tác giả mới</a>
+        <a class="collapse-item" href="{{ route('admin.issued-books') }}">Danh sách tác giả</a>
+      </div>
+    </div>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseSix" aria-expanded="true" aria-controls="collapseSix">
+      <i class="fas fa-boxes"></i>
+      <span>Quản lý nhà xuất bản</span>
+    </a>
+    <div id="collapseSix" class="collapse" aria-labelledby="headingSix" data-parent="#accordionSidebar">
+      <div class="bg-white py-2 collapse-inner rounded">
+        <a class="collapse-item" href="{{ route('admin.issue-book') }}">Thêm thể loại mới</a>
+        <a class="collapse-item" href="{{ route('admin.issued-books') }}">Danh sách thể loại</a>
       </div>
     </div>
   </li>
 
   <!-- Nav Item - Utilities Collapse Menu -->
-  <li class="nav-item">
+  <!-- <li class="nav-item">
     <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
       <i class="fas fa-fw fa-wrench"></i>
       <span>Utilities</span>
@@ -57,32 +117,32 @@
         <a class="collapse-item" href="utilities-other.html">Other</a>
       </div>
     </div>
-  </li>
+  </li> -->
 
   <!-- Divider -->
   <hr class="sidebar-divider">
 
   <!-- Heading -->
   <div class="sidebar-heading">
-    Addons
+    Tiện ích
   </div>
 
   <!-- Nav Item - Pages Collapse Menu -->
   <li class="nav-item">
     <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
       <i class="fas fa-fw fa-folder"></i>
-      <span>Pages</span>
+      <span>Trang</span>
     </a>
     <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
       <div class="bg-white py-2 collapse-inner rounded">
-        <h6 class="collapse-header">Login Screens:</h6>
-        <a class="collapse-item" href="login.html">Login</a>
-        <a class="collapse-item" href="register.html">Register</a>
-        <a class="collapse-item" href="forgot-password.html">Forgot Password</a>
+        <h6 class="collapse-header">Màn hình đăng nhập:</h6>
+        <a class="collapse-item" href="login.html">Đăng nhập</a>
+        <a class="collapse-item" href="register.html">Đăng ký</a>
+        <a class="collapse-item" href="forgot-password.html">Đổi mật khẩu</a>
         <div class="collapse-divider"></div>
-        <h6 class="collapse-header">Other Pages:</h6>
-        <a class="collapse-item" href="404.html">404 Page</a>
-        <a class="collapse-item" href="blank.html">Blank Page</a>
+        <h6 class="collapse-header">Trang khác:</h6>
+        <a class="collapse-item" href="404.html">Trang 404</a>
+        <a class="collapse-item" href="blank.html">Trang trống</a>
       </div>
     </div>
   </li>
@@ -91,14 +151,14 @@
   <li class="nav-item">
     <a class="nav-link" href="charts.html">
       <i class="fas fa-fw fa-chart-area"></i>
-      <span>Charts</span></a>
+      <span>Biểu đồ</span></a>
   </li>
 
   <!-- Nav Item - Tables -->
   <li class="nav-item">
     <a class="nav-link" href="tables.html">
       <i class="fas fa-fw fa-table"></i>
-      <span>Tables</span></a>
+      <span>Bảng</span></a>
   </li>
 
   <!-- Divider -->
